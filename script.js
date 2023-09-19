735329f0d53b49ac67c2d89880526c03608a49e7
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-19 20:02:08 +0530

    now starting building logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// game Constants & variable
 let direction = { x: 0, y: 0 };
 const foodSound = new Audio('music/food.mp3')
 const gameSound = new Audio('music/gameover.mp3')
 const moveSound = new Audio('music/move.mp3')
 const musicSound = new Audio('music/music.mp3')
 
+
+// game function
+function main(current_time) {
+
+}
+
+
+// main logic start here
+window.requestAnimationFrame(main)
