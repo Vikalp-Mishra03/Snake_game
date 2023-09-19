448e4152c10b577bccf5aa3a6b2608f2b903250d
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-19 21:53:08 +0530

    logic for snake collide complete

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // game Constants & variable
-let direction = { x: 0, y: 0 };
+let inputDir = { x: 0, y: 0 };
 const foodSound = new Audio('music/food.mp3')
-const gameSound = new Audio('music/gameover.mp3')
+const gameOverSound = new Audio('music/gameover.mp3')
 const moveSound = new Audio('music/move.mp3')
 const musicSound = new Audio('music/music.mp3')
 let speed = 2;
+let score = 0;
 let lastPaintTime = 0;
 let SnakeArr = [
     { x: 13, y: 15 }
@@ -23,11 +24,18 @@ function main(current_time) {
 
 function gameEngine() {
     // *Part 1: Updating the snake array and food
-
-
-    // *Part 2: Display the snake and food
-    // ?Display the snake
-    board.innerHTML = "";
+    if (isCollide(SnakeArr)){
+        gameOverSound.play()
+        musicSound.pause()
+        inputDir = {x:0, y:0}
+        alert("Game Over Press any key to restart the game!")
+        SnakeArr = [{ x: 13, y: 15 }]
+        musicSound.play()
+        score = 0;
+    }
+        // *Part 2: Display the snake and food
+        // ?Display the snake
+        board.innerHTML = "";
     SnakeArr.forEach((e, index) => {
         snakeElement = document.createElement('div')
         snakeElement.style.gridRowStart = e.y;
