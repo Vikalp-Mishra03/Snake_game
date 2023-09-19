be61206ac95b23d84c3c4acf517fe852cb2c04ec
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-19 20:10:06 +0530

    creating gameEngine function

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const foodSound = new Audio('music/food.mp3')
 const gameSound = new Audio('music/gameover.mp3')
 const moveSound = new Audio('music/move.mp3')
 const musicSound = new Audio('music/music.mp3')
-
-
+let speed = 2;
+let lastPaintTime = 0;
 // game function
 function main(current_time) {
-
+    window.requestAnimationFrame(main);
+    if((current_time - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    console.log(current_time)
+    gameEngine();
 }
 
 
